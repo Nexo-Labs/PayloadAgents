773fc91f904ba5f483540521016a196422b2b0c2
Fiser <ruben@nexolabs.xyz>
2026-01-17 20:25:21 +0100

    feat: added payload-taxonomies and payload-lexical-blocks-builder

---
+++ packages/payload-lexical-blocks-builder/src/renderer.ts
@@ -0,0 +1,32 @@
+import { SerializedEditorState } from "@payloadcms/richtext-lexical/lexical";
+import type { ArgTypes } from "@storybook/react";
+
+export interface LexicalBlockProps<T> {
+  node: {
+    fields: T;
+  };
+}
+export type BlockRendererFunction = ({
+  node,
+}: LexicalBlockProps<any>) => Promise<any> | any;
+export type BlocksRendererFunctions<T extends string> = Record<
+  T,
+  BlockRendererFunction
+>;
+
+export interface GenericStory<P> {
+  argTypes?: Partial<ArgTypes<{ node: { fields: P } }>>;
+}
+
+export type StoryArgs<T> = T extends GenericStory<infer P> ? P : never;
+
+export const generateStoryForLexicalBlock = <T extends GenericStory<any>>(
+  args: StoryArgs<T>
+): { args: LexicalBlockProps<StoryArgs<T>> } => ({
+  args: {
+    node: { fields: args },
+  },
+});
+export type ExtendedSerializedEditorState = SerializedEditorState & {
+    [k: string]: unknown;
+};
---
+++ packages/payload-taxonomies/src/processor/index.mjs
@@ -0,0 +1,9 @@
+import { taxonomizeArticles } from "./taxonomizer.mjs";
+import { processTaxonomyWeights } from "./processor.mjs";
+import { generateRelationshipDiagram } from "./diagram-generator.mjs";
+
+export {
+  taxonomizeArticles,
+  processTaxonomyWeights,
+  generateRelationshipDiagram,
+};
